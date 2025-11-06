e7008628e3ef0199b5039be0cc7c188d99770851
5pedrowx1 <73367665+5pedrowx1@users.noreply.github.com>
2025-11-06 12:23:44 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,13 +4,13 @@ set -e
 # Aceita o EULA
 echo "eula=true" > eula.txt
 
-# Escolhe a versão mais recente do Fabric
+# Baixa a versão mais recente do instalador Fabric
 FABRIC_INSTALLER_URL="https://meta.fabricmc.net/v2/versions/installer"
-FABRIC_INSTALLER_VERSION=$(curl -s $FABRIC_INSTALLER_URL | jq -r '.[0].version')
-curl -O https://meta.fabricmc.net/v2/versions/installer/${FABRIC_INSTALLER_VERSION}/fabric-installer-${FABRIC_INSTALLER_VERSION}.jar
+LATEST_INSTALLER=$(curl -s $FABRIC_INSTALLER_URL | grep -oP '"version":"\K[^"]+' | head -n 1)
+curl -O "https://meta.fabricmc.net/v2/versions/installer/${LATEST_INSTALLER}/fabric-installer-${LATEST_INSTALLER}.jar"
 
 # Instala o servidor para Minecraft 1.21.1
-java -jar fabric-installer-${FABRIC_INSTALLER_VERSION}.jar server -mcversion 1.21.1 -downloadMinecraft
+java -jar fabric-installer-${LATEST_INSTALLER}.jar server -mcversion 1.21.1 -downloadMinecraft
 
 # Inicia o servidor
 java -Xmx2G -Xms2G -jar fabric-server-launch.jar nogui
